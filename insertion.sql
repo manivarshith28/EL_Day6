b3ff24de9a1e34db7182fed03f2554438448cfc3
manivarshith28 <manivarshith2806@gmail.com>
2025-07-01 19:34:57 +0530

    inserting values into emp, dept, & student tables

---
+++ insertion.sql
@@ -0,0 +1,28 @@
+
+Table : Employee
+
+INSERT INTO emp (eid, ename, address) VALUES
+(1, 'Ram', 'Delhi'),
+(2, 'Amrit', 'Pune'),
+(3, 'Ram', 'Banglore'),
+(4, 'Amrit', 'Delhi'),
+(5, 'Ravi', 'Pune'),
+(6, 'Nithin', 'Mumbai'),
+(7, 'Varun', 'Hyderabad');
+
+Table : Department
+
+INSERT INTO dept (did, dname, eid) VALUES
+('d1', 'HR', 1),
+('d2', 'IT', 2),
+('d3', 'MRKT', 3),
+('d4', 'Testing', 4);
+
+Table : Student
+
+INSERT INTO student (sid, sname, role, intern_salary) VALUES
+(1, 'Ram', 'HR', 10000),
+(2, 'Amrit', 'MRKT', 20000),
+(3, 'Ravi', 'HR', 30000),
+(4, 'Nithin', 'MRKT', 40000),
+(5, 'Varun', 'IT', 50000);
