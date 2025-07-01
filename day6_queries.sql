938ed9dcea6ab357df624df869481f374e7a2652
manivarshith28 <manivarshith2806@gmail.com>
2025-07-01 19:39:45 +0530

    Update day6_queries.sql

--- day6_queries.sql
+++ day6_queries.sql
@@ -1,5 +1,6 @@
 use day6;
 
+
 -- Finding employees who work in a department
 -- Method 1 : 
 select eid, ename from emp where exists 
@@ -14,17 +15,20 @@ select eid, ename from emp where eid in
 	select eid from dept
 );
 
+
 -- details of a student with max intern_salary 
 select * from student where intern_salary = 
 (
 	select max(intern_salary) from student
 );
 
+
 -- 2nd highest intern_salary
 select max(intern_salary) from student where intern_salary <> 
 (
 	select max(intern_salary) from student
 );
+
 
 -- details of a student with 2nd highest intern_salary
 select * from student where intern_salary = 
@@ -35,9 +39,11 @@ select * from student where intern_salary =
 	)
 );
 
+
 -- no.of students working in a role
 select role as Department, count(*) as No_of_Employees 
 from student group by role;
+
 
 -- roles with less than two working students & their details
 select sname, role from student where role in 
@@ -45,11 +51,13 @@ select sname, role from student where role in
 	select role from student group by role having count(*) < 2
 );
 
+
 -- roles with exactly two working students & their details
 select sname, role from student where role in
 (
 	select role from student group by role having count(*) = 2
 );
+
 
 -- highest inter_salary role wise
 select sname, role, intern_salary from student where intern_salary in 
